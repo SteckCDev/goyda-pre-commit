7d12f8030e92548216db70a075e8fc8ac2080d78
Ilya Tarasov <griefbotlife@gmail.com>
2024-12-01 14:05:10 +0300

    Testing hook languages

--- setup.py
+++ setup.py
@@ -3,6 +3,11 @@ from setuptools import setup
 
 setup(
     name="goyda-pre-commit-hook",
-    version="0.6.1",
+    version="0.6.2",
     py_modules=["goyda"],
+    entry_points={
+        "console_scripts": [
+            "goyda-hook=goyda:main",
+        ],
+    },
 )
